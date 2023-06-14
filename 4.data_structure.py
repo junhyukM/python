9ad853aecef9c3493eab57728a4b61a8f5755a19
junhyukM <minjh1031@gmail.com>
2023-06-14 15:38:42 +0900

    update

--- 4.data_structure.py
+++ 4.data_structure.py
@@ -116,7 +116,7 @@ numbers.reverse()
 
 # copy
 # 같은 주소를 참조한다.
-# 리스트에 해당! 변수는 아니다. 근데 다른 자료구조는 어떤지 모른다. 알아봐야될듯
+# 리스트에 해당! 변수는 아니다.
 # origin_list = [1, 2, 3]
 # copy_list = origin_list
 
@@ -165,6 +165,202 @@ b[2][0] = 100
 a = [1, 2, 3, 4]
 a.clear()
 # print(a)
+
+
+# list comprehension
+
+numbers = list(range(1, 11))
+
+# 세제곱 만들기 for
+result = []
+for i in numbers:
+    result.append(i**3)
+
+# print(result)
+# 세제곱 만들기 comp
+result2 = [i**3 for i in numbers]
+# print(result2)
+
+
+# 짝수만 고르기 for
+even_list = []
+for i in numbers:
+    if i % 2 == 0:
+        even_list.append(i)
+# print(even_list)
+
+# 짝수만 고르기 comp
+even_list2 = [i for i in numbers if i % 2 == 0]
+# print(even_list2)
+
+
+# 연습
+# 모음을 제거
+words = 'my name is Min'
+vowels = 'aeiou'
+
+# 반복문 버전
+result = []
+for c in words:
+    if c not in vowels:
+        result.append(c)
+        
+# print(''.join(result))
+
+# comp 버전
+result2 = [c for c in words if c not in vowels]
+# print(''.join(result2))
+
+
+# 3. 딕셔너리 메소드
+
+info = {
+    'name': 'Min',
+    'location': 'incheon',
+    'phone': '123-123-123'
+}
+
+
+# .pop(key[, default])
+info.pop('phone')
+# print(info)
+# print(info.pop('school', 'key가 없습니다.'))
+# print(info)
+
+
+# .update(key=value)
+info.update(name='junhyuk')
+# print(info)
+
+
+# .get(key[, default])
+# print(info['name'])
+# print(info.get('name'))
+# print(info['school'])
+# print(info.get('school'))
+
+
+# dictionary comprehension
+# {1: 1, 2: 8, 3: 27}
+cube_dict = {}
+for i in range(1, 4):
+    cube_dict[i] = i**3
+# print(cube_dict)
+
+cube_dict2 = {i: i**3 for i in range(1, 4)}
+# print(cube_dict2)
+
+
+# 연습
+dust = {
+    '서울': 100,
+    '대전': 12,
+    '인천': 70,
+    '부산': 34,
+    '전주': 67,
+}
+
+# 50 이상 지역만 뽑아서 새로운 dict 만들기
+# for
+l_dict = {}
+for l, d in dust.items():
+    if d >= 50:
+        l_dict[l] = d
+# print(l_dict)        
+
+# comp 
+l_dict2 = {l: d for l, d in dust.items() if d >= 50}
+# print(l_dict2)
+
+result3 = {k: '나쁨' if v >= 50 else '좋음' for k, v in dust.items()}
+# print(result3)
+
+
+# 4. 세트 메소드
+
+fruits = {'apple', 'banana', 'melon'}
+
+# .add(x)
+fruits.add('watermelon')
+# print(fruits)
+
+# .update(*objects)
+fruits.update('grape')
+fruits.update({'orange', 'pear'})
+# print(fruits)
+
+
+# .remove(x)
+fruits.remove('banana')
+# print(fruits)
+# 없을 때 제거하려면 에러 
+# fruits.remove('dog')
+
+# .discard(x)]
+# 없어도 에러 안뜸
+fruits.discard('dof')
+# print(fruits)
+
+
+# .pop()
+# 임의의 원소 제거
+# fruits.pop()
+# print(fruits)
+
+
+
+# 5. map(), zip(), filter()
+
+# map(function, iterable 데이터)
+number = [1, 2, 3]
+
+number_str = map(str, number)
+# print(number_str)
+# print(list(number_str))
+
+def cube(x):
+    return x**3
+
+cube_list = list(map(cube, number))
+# print(cube_list)
+
+
+# 1 2 3 4 5 6 7 8 9 10 => [1, 2, 3, 4, 5...]
+# => 1 8 27 ... 1000
+
+# numbers = input().split()
+# result = map(int, numbers)
+# print(list(result))
+
+# numbers = list(map(int, input().split()))
+# print(list(numbers))
+
+
+# zip
+a_number = [1, 2, 3]
+b_number = [100, 200, 300]
+
+# print(list(zip(a_number, b_number)))
+
+
+
+# filter(function, iterable)
+# function은 참/거짓이 반환되는 함수여야 한다.
+
+def isodd(x):
+#     if x % 2 == 1:
+#         return True
+#     else:
+#         return False
+    return bool(x % 2) 
+
+numbers = [1, 2, 3, 4, 5]
+result = filter(isodd, numbers)
+print(list(result))
+
+
+
+
 
 
 
