777f76c20b4b6d1ce87966082634cd8c643fee88
junhyukM <minjh1031@gmail.com>
2024-03-12 22:50:00 +0900

    first commit

--- programmers.py
+++ programmers.py
@@ -26,4 +26,34 @@ def solution(my_string):
     return answer
 
 
-# 다음 문제
+# 입문 문제 - 진료 순서 정하기
+# 외과의사 머쓱이는 응급실에 온 환자의 응급도를 기준으로 진료 순서를 정하려고 합니다. 
+# 정수 배열 emergency가 매개변수로 주어질 때 응급도가 높은 순서대로 진료 순서를 정한 배열을 return하도록 solution 함수를 완성해주세요.
+'''
+제한사항
+중복된 원소는 없습니다.
+1 ≤ emergency의 길이 ≤ 10
+1 ≤ emergency의 원소 ≤ 100
+'''
+
+def solution(emergency):
+    answer = []
+    emergency_dict = {}
+    for idx, n in enumerate(emergency):
+        emergency_dict[n] = idx
+        
+    temp2 = list(emergency_dict.values())
+    temp = list(emergency_dict.keys())
+    
+    temp.sort(reverse=True)
+    
+    num = 1
+    for i in temp:
+       
+        emergency_dict[i] = num
+        num += 1
+        
+    answer = list(emergency_dict.values())
+    return answer
+
+
