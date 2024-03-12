d8f00ad21eca3b49c6e06f58abb7d06bdb1ccc64
junhyukM <minjh1031@gmail.com>
2024-03-12 23:43:05 +0900

    second commit

--- programmers.py
+++ programmers.py
@@ -57,3 +57,30 @@ def solution(emergency):
     return answer
 
 
+# 입문 문제 - 한 번만 등장한 문자
+# 문자열 s가 매개변수로 주어집니다. s에서 한 번만 등장하는 문자를 사전 순으로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요. 
+# 한 번만 등장하는 문자가 없을 경우 빈 문자열을 return 합니다.
+
+'''
+제한사항
+0 < s의 길이 < 1,000
+s는 소문자로만 이루어져 있습니다.
+'''
+
+def solution(s):
+    answer = ''
+    list = []
+    b_list = []
+    total_list = []
+    for i in s:
+        if i not in list:
+            list.append(i)
+        else:
+            b_list.append(i)
+            
+    for l in s:
+        if l not in b_list:
+            total_list.append(l)
+            total_list.sort()
+    answer = ''.join(total_list)
+    return answer
