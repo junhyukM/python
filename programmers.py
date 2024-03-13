67d9c00048e3babc4e41c7223dc4c63ca7db7b55
junhyukM <minjh1031@gmail.com>
2024-03-13 23:06:03 +0900

    0313

--- programmers.py
+++ programmers.py
@@ -84,3 +84,51 @@ def solution(s):
             total_list.sort()
     answer = ''.join(total_list)
     return answer
+
+
+# 입문 문제 - 7의 개수
+# 머쓱이는 행운의 숫자 7을 가장 좋아합니다. 정수 배열 array가 매개변수로 주어질 때, 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
+
+def solution(array):
+    answer = 0
+    word = ''.join(str(e) for e in array)
+    for i in word:
+        if int(i) == 7:
+            answer += 1
+    return answer
+
+
+
+# 입문 문제 - 컨트롤 제트
+# 숫자와 "Z"가 공백으로 구분되어 담긴 문자열이 주어집니다. 문자열에 있는 숫자를 차례대로 더하려고 합니다. 
+# 이 때 "Z"가 나오면 바로 전에 더했던 숫자를 뺀다는 뜻입니다. 숫자와 "Z"로 이루어진 문자열 s가 주어질 때, 
+# 머쓱이가 구한 값을 return 하도록 solution 함수를 완성해보세요.
+
+'''
+s는 숫자, "Z", 공백으로 이루어져 있습니다.
+s에 있는 숫자와 "Z"는 서로 공백으로 구분됩니다.
+연속된 공백은 주어지지 않습니다.
+0을 제외하고는 0으로 시작하는 숫자는 없습니다.
+s는 "Z"로 시작하지 않습니다.
+s의 시작과 끝에는 공백이 없습니다.
+"Z"가 연속해서 나오는 경우는 없습니다.
+'''
+
+def solution(s):
+    answer = 0
+    s = s.split()
+    
+    num_list = []
+
+    for i in s:
+        try :
+            if float(i):
+                num_list.append(int(i))
+        except:
+            
+            if num_list:
+                num_list.pop(-1)
+            
+    answer = sum(num_list)    
+
+    return answer
