1707cbc683044d229b34bcebe36154aed31975a5
junhyukM <minjh1031@gmail.com>
2024-03-18 23:18:05 +0900

    0318

--- programmers.py
+++ programmers.py
@@ -187,3 +187,24 @@ def solution(n):
     return answer
 
 
+# 입문 문제 - 잘라서 배열로 저장하기
+# 문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
+
+'''
+1 ≤ my_str의 길이 ≤ 100
+1 ≤ n ≤ my_str의 길이
+my_str은 알파벳 소문자, 대문자, 숫자로 이루어져 있습니다.
+'''
+
+def solution(my_str, n):
+    answer = []
+    while True:
+        if len(my_str) > n:
+            answer.append(my_str[:n])
+            my_str = my_str[n:]
+        else:
+            answer.append(my_str)
+            break
+    return answer
+
+
