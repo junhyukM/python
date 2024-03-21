5638b5e16c47d7a9f0ae21c65b233cd554f00805
junhyukM <minjh1031@gmail.com>
2024-03-21 23:05:39 +0900

    0321

--- programmers.py
+++ programmers.py
@@ -267,4 +267,29 @@ def solution(keyinput, board):
     return answer
 
 
+# 입문 문제 - 직사각형 넓이 구하기
+# 2차원 좌표 평면에 변이 축과 평행한 직사각형이 있습니다. 
+# 직사각형 네 꼭짓점의 좌표 [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]가 담겨있는 배열 dots가 매개변수로 주어질 때, 
+# 직사각형의 넓이를 return 하도록 solution 함수를 완성해보세요.
+
+'''
+dots의 길이 = 4
+dots의 원소의 길이 = 2
+-256 < dots[i]의 원소 < 256
+잘못된 입력은 주어지지 않습니다.
+'''
+
+def solution(dots):
+    answer = 0
+    temp = dots[0]
+    x_len = 0
+    y_len = 0
+    for i in dots[1:]:
+        if temp[0] == i[0]:
+            x_len = abs(temp[1] - i[1])
+        elif temp[1] == i[1]:
+            y_len = abs(temp[0] - i[0])
+    answer = x_len * y_len    
+    
+    return answer
 
