09e41772775c6974f36823e0fdc0dea2b9b1a412
junhyukM <minjh1031@gmail.com>
2024-03-25 20:59:03 +0900

    0325

--- programmers.py
+++ programmers.py
@@ -327,3 +327,30 @@ def solution(id_pw, db):
         answer = 'fail'
             
     return answer
+
+
+
+# 입문 문제 - 등수 매기기
+# 영어 점수와 수학 점수의 평균 점수를 기준으로 학생들의 등수를 매기려고 합니다. 영어 점수와 수학 점수를 담은 2차원 정수 배열 score가 주어질 때, 
+# 영어 점수와 수학 점수의 평균을 기준으로 매긴 등수를 담은 배열을 return하도록 solution 함수를 완성해주세요.
+
+'''
+0 ≤ score[0], score[1] ≤ 100
+1 ≤ score의 길이 ≤ 10
+score의 원소 길이는 2입니다.
+score는 중복된 원소를 갖지 않습니다.
+'''
+
+def solution(score):
+    answer = []
+    temp = []
+    for i in score:
+        temp.append(sum(i))
+    
+    rank_list = sorted(temp, reverse=True)
+    
+    for i in temp: 
+        answer.append(rank_list.index(i)+1)
+        
+    return answer
+
